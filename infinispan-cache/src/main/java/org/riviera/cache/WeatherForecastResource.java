f2010b42f4133627a8be8f921da9e942cf7578ed
Katia Aresti <karesti@redhat.com>
2024-07-09 13:02:46 +0200

    cleanup

--- infinispan-cache/src/main/java/org/riviera/cache/WeatherForecastResource.java
+++ infinispan-cache/src/main/java/org/riviera/cache/WeatherForecastResource.java
@@ -1,22 +1,13 @@
 package org.riviera.cache;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import org.jboss.resteasy.reactive.RestQuery;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-
-import io.quarkus.logging.Log;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.context.Dependent;
-import jakarta.inject.Inject;
-import jakarta.json.Json;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-
-import jakarta.ws.rs.core.Response;
-import org.infinispan.client.hotrod.exceptions.HotRodClientException;
-import org.jboss.resteasy.reactive.RestQuery;
-import org.jboss.resteasy.reactive.RestResponse;
-import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
 @Path("/weather")
 public class WeatherForecastResource {
@@ -37,48 +28,5 @@ public class WeatherForecastResource {
                 service.getDailyForecastParis(nowPlusDaysPlus2.toEpochDay()));
         long executionEnd = System.currentTimeMillis();
         return new WeatherForecast(dailyForecasts, executionEnd - executionStart);
-    }
-
-//    @GET
-//    public WeatherForecast getForecast(@RestQuery String city, @RestQuery long daysInFuture) {
-//        long executionStart = System.currentTimeMillis();
-//        LocalDate nowPlusDays = LocalDate.now().plusDays(daysInFuture);
-//        LocalDate nowPlusDaysPlus1 = LocalDate.now().plusDays(daysInFuture + 1L);
-//        LocalDate nowPlusDaysPlus2 = LocalDate.now().plusDays(daysInFuture + 2L);
-//        List<String> dailyForecasts = Arrays.asList(
-//                service.getDailyForecastDayMonth(nowPlusDays.toEpochDay(), city),
-//                service.getDailyForecastDayMonth(nowPlusDaysPlus1.toEpochDay(), city),
-//                service.getDailyForecastDayMonth(nowPlusDaysPlus2.toEpochDay(), city));
-//        long executionEnd = System.currentTimeMillis();
-//        return new WeatherForecast(dailyForecasts, executionEnd - executionStart);
-//    }
-
-    @GET
-    public WeatherForecast getForecast(@RestQuery String city, @RestQuery long daysInFuture) {
-        long executionStart = System.currentTimeMillis();
-        LocalDate nowPlusDays = LocalDate.now().plusDays(daysInFuture);
-        LocalDate nowPlusDaysPlus1 = LocalDate.now().plusDays(daysInFuture + 1L);
-        LocalDate nowPlusDaysPlus2 = LocalDate.now().plusDays(daysInFuture + 2L);
-        List<Weather> dailyForecasts = Arrays.asList(
-                service.getDaily(nowPlusDays.toEpochDay(), city),
-                service.getDaily(nowPlusDaysPlus1.toEpochDay(), city),
-                service.getDaily(nowPlusDaysPlus2.toEpochDay(), city));
-        long executionEnd = System.currentTimeMillis();
-        return new WeatherForecast(dailyForecasts, executionEnd - executionStart);
-    }
-
-    @GET
-    @Path("travel")
-    public List<String> getTravelCity(@RestQuery String type) {
-        Log.info(type);
-        return service.searchCityByWeather(type);
-    }
-
-    public static class ExceptionMappers {
-        @ServerExceptionMapper
-        public RestResponse<String> mapException(HotRodClientException ex) {
-            return RestResponse.status(Response.Status.BAD_REQUEST,
-                    Json.createObjectBuilder().add("infinispan-error", ex.getMessage()).build().toString());
-        }
     }
 }
--- infinispan-cache/src/main/java/org/riviera/cache/WeatherForecastService.java
+++ infinispan-cache/src/main/java/org/riviera/cache/WeatherForecastService.java
@@ -1,56 +1,22 @@
 package org.riviera.cache;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import io.quarkus.infinispan.client.Remote;
-import io.quarkus.logging.Log;
+import io.quarkus.cache.CacheResult;
 import jakarta.enterprise.context.ApplicationScoped;
 
-import io.quarkus.cache.CacheResult;
-import jakarta.inject.Inject;
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.commons.api.query.Query;
+import java.time.LocalDate;
 
 @ApplicationScoped
 public class WeatherForecastService {
 
-    @CacheResult(cacheName = "parisbackups")
+    @CacheResult(cacheName = "mycache")
     public String getDailyForecastParis(long epoch) {
-//        try {
-//            Thread.sleep(1000L);
-//        } catch (InterruptedException e) {
-//            Thread.currentThread().interrupt();
-//        }
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         LocalDate localDate = LocalDate.ofEpochDay(epoch);
         return localDate.getDayOfWeek() + " will be " + getDailyResult(localDate.getDayOfMonth() % 4);
-    }
-
-    @CacheResult(cacheName = "allcities")
-    public String getDailyForecastDayMonth(long epoch, String city) {
-        LocalDate localDate = LocalDate.ofEpochDay(epoch);
-        return localDate.getDayOfWeek() + " will be " + getDailyResult(localDate.getDayOfMonth() % 4) + " in " + city;
-    }
-
-    @CacheResult(cacheName = "allcities")
-    public Weather getDaily(long epoch, String city) {
-        LocalDate localDate = LocalDate.ofEpochDay(epoch);
-        String dailyResult = getDailyResult(localDate.getDayOfMonth() % 4);
-        return new Weather(dailyResult, localDate.getDayOfWeek().name(), city);
-    }
-
-    @Inject
-    @Remote("allcities")
-    private RemoteCache<String, Weather> weatherRemoteCache;
-
-    public List<String> searchCityByWeather(String weather) {
-        Query<Object[]> query = weatherRemoteCache.query("select w.city from riviera.Weather w where w.weather:':p1'~2");
-//        Query<Object[]> query = weatherRemoteCache.query("select w.city from riviera.Weather w where w.weather=':p1'");
-        query.setParameter("p1", weather);
-        Log.info(query.getQueryString());
-        query.execute().list().stream().forEach(s -> System.out.println(s));
-        return query.execute().list().stream().map(r -> r[0].toString()).collect(Collectors.toList());
     }
 
     private String getDailyResult(int dayOfMonthModuloFour) {
